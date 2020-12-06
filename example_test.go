6437cef3b61735f066da96a9fdba036845664376
Oleg Balunenko <oleg.balunenko@gmail.com>
2020-12-07 01:18:58 +0300

    Add gomod and vendor; Fix testable examples signature
    - Add go.mod and vendor to have reproducible versions
    - Fix testable examples signature according to go doc https://golang.org/pkg/testing/#hdr-Examples

--- example_test.go
+++ example_test.go
@@ -1,27 +1,48 @@
 package prompt_test
 
 import (
+	"fmt"
 	"net/mail"
 
-	"github.com/Bowery/prompt"
 	"golang.org/x/crypto/bcrypt"
+
+	"github.com/Bowery/prompt"
 )
 
-func ExamplePassword() ([]byte, error) {
+func ExamplePassword() {
 	clear, err := prompt.Password("Password")
 	if err != nil {
-		return nil, err
+		// handle error
+		return
 	}
 
-	return bcrypt.GenerateFromPassword([]byte(clear), bcrypt.DefaultCost)
+	password, err := bcrypt.GenerateFromPassword([]byte(clear), bcrypt.DefaultCost)
+	if err != nil {
+		// handle error
+		return
+	}
+	fmt.Println(password)
+	// Output:
+	fmt.Println(err)
+	// Output:
+
+	return
 }
 
-func ExampleBasic() (string, error) {
+func ExampleBasic() {
 	email, err := prompt.Basic("Email", true)
 	if err != nil {
-		return "", err
+		// handle error
+		return
 	}
 
 	_, err = mail.ParseAddress(email)
-	return email, err
+	if err != nil {
+		// handle error
+		return
+	}
+	fmt.Println(email)
+	// Output:
+	fmt.Println(err)
+	// Output:
 }
